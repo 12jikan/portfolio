066788d9715848449603d5ecd10268b29a87e88b
12jikan <jareem.e.hoff@gmail.com>
2019-03-30 16:26:59 -0400

    added the navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from '../core/modules/app-routing.module';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavComponent],
   imports: [
     CommonModule,
     AppRoutingModule
